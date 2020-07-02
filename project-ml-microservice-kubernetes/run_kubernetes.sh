ad23512894d79b90f6d39f7b31fb82966ead7d8b
a-nader <65056261+a-nader@users.noreply.github.com>
2020-07-02 02:15:15 +0200

    Add files via upload

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=anadersalem/mlms-docker-pro:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+#kubectl run --image $dockerpath mlms-docker-pro --port=80
+kubectl create deployment mlms-docker-pro --image=anadersalem/mlms-docker-pro
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
 
+kubectl port-forward deployment/mlms-docker-pro 8000:80
